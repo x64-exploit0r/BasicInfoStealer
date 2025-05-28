9bc40e9f073aa74b269794c4a9ddb2ca5736a15d
x64-exploit0r <kumararyan4927@gmail.com>
2025-05-28 18:07:06 +0530

    Minor changes

--- Sour2ce.cpp
+++ Sour2ce.cpp
@@ -12,23 +12,25 @@ void GetSystemInfo() {
 	char username[UNLEN + 1];
 	char ComputerName[UNLEN + 1];
 	char CurrentDirectory[UNLEN + 1];
+	char Username[UNLEN + 1];
 	LPSTR Date;
 
 	DWORD size = UNLEN + 1;
 	DWORD ComputerNameSize = MAX_COMPUTERNAME_LENGTH + 1;
 	DWORD CurrentDirectorySize = UNLEN + 1;
 	DWORD DateSize;
+	DWORD UsernameSize;
 
 	GetUserNameA(username, &size);
 	GetComputerNameA(ComputerName, &ComputerNameSize);
 	GetCurrentDirectoryA(CurrentDirectorySize, CurrentDirectory);
-	GetDateFormatA(LOCALE_USER_DEFAULT, DATE_SHORTDATE, NULL, "ddd',' MMM dd yy", &Date, DateSize);
+	GetUserNameA(Username, &UsernameSize);
 	
 
 	cout << pos << "GOT USERNAME - " << username << endl;
 	cout << pos << "GOT COMPUTER NAME - " << ComputerName << endl;
 	cout << pos << "GOT CURRENT DIRECTORY - " << CurrentDirectory<< endl;
-	cout << pos << "GOT DATE - "<< Date << endl;
+	cout << pos << "GOT USERNAME - " << Username << endl;
 	
 
 
